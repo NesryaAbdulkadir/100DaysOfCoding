2ef7a221c2789e7eae28a5b33317844780cc4401
NesryaAbdulkadir <nesryaabdulkadir.dev@gmail.com>
2024-11-30 22:18:14 +0300

    Minor Adjustment

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -2,7 +2,15 @@ import PopularMovies from "./components/PopularMovies";
 import Routes from "./Routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <div>
+      <Routes />;
+      <p className="text-center text-sm text-gray-400 p-5">
+        "This website uses TMDB and the TMDB APIs but is not endorsed,
+        certified, or otherwise approved by TMDB."
+      </p>
+    </div>
+  );
 }
 
 export default App;
